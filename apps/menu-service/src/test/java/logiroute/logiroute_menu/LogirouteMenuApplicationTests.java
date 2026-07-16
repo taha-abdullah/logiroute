b00d6c3b14da4b9af4fcb0ff87cfcd5ecaada174
taha <taha.abdullah20@gmail.com>
2026-07-16 23:13:17 +0200

    Initialize logiroute menu serice

---
+++ apps/menu-service/src/test/java/logiroute/logiroute_menu/LogirouteMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package logiroute.logiroute_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogirouteMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
