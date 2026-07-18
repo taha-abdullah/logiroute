408255e9142edaaee6e424f35ed0beeb5850b97c
Taha Abdullah <30592701+taha-abdullah@users.noreply.github.com>
2026-07-19 01:25:35 +0200

    feat(menu-service): initialize Spring Boot menu service (#2)
    * Initialize logiroute menu serice
    
    * fix(menu-service): remove invalid test dependencies from pom.xml
    
    * fix(menu-service): add missing maven wrapper config and fix context-load test
    
    * fix(menu-service): replace invalid webmvc starter with spring-boot-starter-web

---
+++ apps/menu-service/src/main/java/logiroute/logiroute_menu/LogirouteMenuApplication.java
@@ -0,0 +1,13 @@
+package logiroute.logiroute_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogirouteMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogirouteMenuApplication.class, args);
+	}
+
+}
