22897c7d4e4426565d7f8c4669afc9c03061d694
taha <taha.abdullah20@gmail.com>
2026-07-20 03:55:05 +0200

    chore(delivery-service): initial scaffold

---
+++ apps/delivery-service/src/main/java/logiroute/logiroute_delivery/LogirouteDeliveryApplication.java
@@ -0,0 +1,13 @@
+package logiroute.logiroute_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogirouteDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogirouteDeliveryApplication.class, args);
+	}
+
+}
